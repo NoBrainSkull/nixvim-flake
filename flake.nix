10cf1f5c9cd2542b5f7ea0c6002e5258613c7c45
cprice <cprice@tutanota.com>
2025-04-01 16:55:15 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = { nixvim, flake-parts, ... }@inputs: {
       nixosModules.nixvim = nixvim.legacyPackages.x86_64-linux.makeNixvimWithModule {
-        module = import ./config;
+        module = { ...}: { imports = [ ./config ]; };
         extraSpecialArgs = {
           inherit inputs;
         };
