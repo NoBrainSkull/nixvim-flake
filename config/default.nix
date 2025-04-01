60e956346667e412fae5b5ee39733fe0ddf68ff3
cprice <cprice@tutanota.com>
2025-04-01 15:42:56 +0200

    fix: [flake] add package as nixosModule

--- config/default.nix
+++ config/default.nix
@@ -1,253 +1,251 @@
 {self, ...}: {
-  # Import all your configuration modules here
-  imports = [];
+# Import all your configuration modules here
+	imports = [];
 
-  config = {
-    opts = {
-      # numbering
-      number = true;
-      relativenumber = true;
+	config = {
+		globals.mapleader = " ";
+		opts = {
+# numbering
+			number = true;
+			relativenumber = true;
 
-      # clipboard
-      clipboard = [ "unnamedplus" ];
+# clipboard
+			clipboard = [ "unnamedplus" ];
 
-      # indentation
-      tabstop = 2;
-      softtabstop = 2;
-      shiftwidth = 2;
-      expandtab = true;
-      smartindent = true;
+# indentation
+			tabstop = 2;
+			softtabstop = 2;
+			shiftwidth = 2;
+			expandtab = true;
+			smartindent = true;
 
-      # swap, backup, undo
-      swapfile = false;
-      backup = false;
-      undofile = false;
+# swap, backup, undo
+			swapfile = false;
+			backup = false;
+			undofile = false;
 
-      # search
-      incsearch = true;
-      hlsearch = true;
-      ignorecase = true;
-      smartcase = true;
+# search
+			incsearch = true;
+			hlsearch = true;
+			ignorecase = true;
+			smartcase = true;
 
-      # code folding
-      foldcolumn = "0";
-      foldlevel = 99;
-      foldlevelstart = 99;
-      foldenable = true;
+# code folding
+			foldcolumn = "0";
+			foldlevel = 99;
+			foldlevelstart = 99;
+			foldenable = true;
 
-      # misc
-      wrap = false;
-      scrolloff = 8;
-      cursorline = true;
-      completeopt = "menu,menuone,noselect";
-    };
+# misc
+			wrap = false;
+			scrolloff = 8;
+			cursorline = true;
+			completeopt = "menu,menuone,noselect";
 
-    globals.mapleader = " ";
 
-    keymaps = [
-      {
-        mode = ["i"];
-        key = "jk";
-        action = "<esc>";
-      }
-      {
-        mode = ["n" "v"];
-        key = "<leader>fe";
-        action = ":Telescope file_browser path=%:p:h select_buffer=true<cr>";
-      }
-      {
-        mode = ["n" "v"];
-        key = "<leader>ff";
-        action = ":Telescope find_files<cr>";
-      }
-      {
-        mode = ["n" "v"];
-        key = "<leader>fg";
-        action = ":Telescope live_grep<cr>";
-      }
-      {
-        mode = ["n" "v"];
-        key = "<leader>fb";
-        action = ":Telescope buffers<cr>";
-      }
-      {
-        mode = [
-          "i"
-          "s"
-        ];
-        key = "<C-k>";
-        action.__raw = ''
-          function()
-           local ls = require "luasnip" 
-           if ls.expand_or_jumpable() then
-             ls.expand_or_jump()
-           end
-          end
-        '';
-      }
-      {
-        mode = [
-          "i"
-          "s"
-        ];
-        key = "<C-j>";
-        action.__raw = ''
-          function()
-           local ls = require "luasnip" 
-           if ls.jumpable(-1) then
-             ls.jump(-1)
-           end
-          end
-        '';
-      }
-    ];
+			keymaps = [
+			{
+				mode = ["i"];
+				key = "jk";
+				action = "<esc>";
+			}
+			{
+				mode = ["n" "v"];
+				key = "<leader>fe";
+				action = ":Telescope file_browser path=%:p:h select_buffer=true<cr>";
+			}
+			{
+				mode = ["n" "v"];
+				key = "<leader>ff";
+				action = ":Telescope find_files<cr>";
+			}
+			{
+				mode = ["n" "v"];
+				key = "<leader>fg";
+				action = ":Telescope live_grep<cr>";
+			}
+			{
+				mode = ["n" "v"];
+				key = "<leader>fb";
+				action = ":Telescope buffers<cr>";
+			}
+			{
+				mode = [
+					"i"
+						"s"
+				];
+				key = "<C-k>";
+				action.__raw = ''
+					function()
+					local ls = require "luasnip" 
+					if ls.expand_or_jumpable() then
+						ls.expand_or_jump()
+							end
+							end
+							'';
+			}
+			{
+				mode = [
+					"i"
+						"s"
+				];
+				key = "<C-j>";
+				action.__raw = ''
+					function()
+					local ls = require "luasnip" 
+					if ls.jumpable(-1) then
+						ls.jump(-1)
+							end
+							end
+							'';
+			}
+			];
 
-    colorschemes.catppuccin.enable = true;
+			colorschemes.catppuccin.enable = true;
 
-    plugins.lualine.enable = true;
-    plugins.web-devicons.enable = true;
+			plugins.lualine.enable = true;
+			plugins.web-devicons.enable = true;
 
-    plugins.lsp = {
-      enable = true;
-      servers = {
-        ts_ls.enable = true;
-        nil_ls.enable = true;
-      };
+			plugins.lsp = {
+				enable = true;
+				servers = {
+				};
 
-      	keymaps = {
-          silent = true;
-          lspBuf = {
-            gd = {
-              action = "definition";
-              desc = "Goto Definition";
-            };
-            gr = {
-              action = "references";
-              desc = "Goto References";
-            };
-            gD = {
-              action = "declaration";
-              desc = "Goto Declaration";
-            };
-            gI = {
-              action = "implementation";
-              desc = "Goto Implementation";
-            };
-            gT = {
-              action = "type_definition";
-              desc = "Type Definition";
-            };
-            "<leader>h" = {
-              action = "hover";
-              desc = "Hover";
-            };
-            "<leader>cw" = {
-              action = "workspace_symbol";
-              desc = "Workspace Symbol";
-            };
-            "<leader>cr" = {
-              action = "rename";
-              desc = "Rename";
-            };
-            "<leader>F" = {
-              action = "format";
-              desc = "Format";
-            };
-          };
+				keymaps = {
+					silent = true;
+					lspBuf = {
+						gd = {
+							action = "definition";
+							desc = "Goto Definition";
+						};
+						gr = {
+							action = "references";
+							desc = "Goto References";
+						};
+						gD = {
+							action = "declaration";
+							desc = "Goto Declaration";
+						};
+						gI = {
+							action = "implementation";
+							desc = "Goto Implementation";
+						};
+						gT = {
+							action = "type_definition";
+							desc = "Type Definition";
+						};
+						"<leader>h" = {
+							action = "hover";
+							desc = "Hover";
+						};
+						"<leader>cw" = {
+							action = "workspace_symbol";
+							desc = "Workspace Symbol";
+						};
+						"<leader>cr" = {
+							action = "rename";
+							desc = "Rename";
+						};
+						"<leader>F" = {
+							action = "format";
+							desc = "Format";
+						};
+					};
 
-          diagnostic = {
-            "<leader>e" = {
-              action = "open_float";
-              desc = "Line Diagnostics";
-            };
-            "gl" = {
-              action = "goto_next";
-              desc = "Next Diagnostic";
-            };
-            "gh" = {
-              action = "goto_prev";
-              desc = "Previous Diagnostic";
-            };
-          };
-	     };
-    };
+					diagnostic = {
+						"<leader>e" = {
+							action = "open_float";
+							desc = "Line Diagnostics";
+						};
+						"gl" = {
+							action = "goto_next";
+							desc = "Next Diagnostic";
+						};
+						"gh" = {
+							action = "goto_prev";
+							desc = "Previous Diagnostic";
+						};
+					};
+				};
+			};
 
-    plugins.cmp = {
-      enable = true;
-      autoEnableSources = true;
-      settings = {
-        sources = [
-          {
-            name = "nvim_lsp";
-            priority = 1000;
-          }
-          {
-            name = "nvim_lsp_signature_help";
-            priority = 1000;
-          }
-          {
-            name = "treesitter";
-            priority = 850;
-          }
-          {
-            name = "luasnip";
-            priority = 750;
-          }
-          {
-            name = "buffer";
-            priority = 500;
-          }
-          { name = "path"; priority = 300;}
-          { name = "git"; priority = 250;}
-          { name = "calc"; priority = 250;}
-          { name = "emoji"; priority = 100;}       
-        ];
+			plugins.cmp = {
+				enable = true;
+				autoEnableSources = true;
+				settings = {
+					sources = [
+					{
+						name = "nvim_lsp";
+						priority = 1000;
+					}
+					{
+						name = "nvim_lsp_signature_help";
+						priority = 1000;
+					}
+					{
+						name = "treesitter";
+						priority = 850;
+					}
+					{
+						name = "luasnip";
+						priority = 750;
+					}
+					{
+						name = "buffer";
+						priority = 500;
+					}
+					{ name = "path"; priority = 300;}
+					{ name = "git"; priority = 250;}
+					{ name = "calc"; priority = 250;}
+					{ name = "emoji"; priority = 100;}       
+					];
 
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<Down>" = "cmp.mapping.select_next_item()";
-          "<Up>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<Tab>" = "cmp.mapping.confirm({ select = true })";
-        };
-      };
-    };
+					mapping = {
+						"<CR>" = "cmp.mapping.confirm({ select = true })";
+						"<Down>" = "cmp.mapping.select_next_item()";
+						"<Up>" = "cmp.mapping.select_prev_item()";
+						"<C-e>" = "cmp.mapping.abort()";
+						"<C-d>" = "cmp.mapping.scroll_docs(-4)";
+						"<C-f>" = "cmp.mapping.scroll_docs(4)";
+						"<C-Space>" = "cmp.mapping.complete()";
+						"<Tab>" = "cmp.mapping.confirm({ select = true })";
+					};
+				};
+			};
 
-    plugins.telescope = {
-      enable = true;
-      extensions.fzy-native.enable = true;
-      extensions.file-browser.enable = true;
-    };
+			plugins.telescope = {
+				enable = true;
+				extensions.fzy-native.enable = true;
+				extensions.file-browser.enable = true;
+			};
 
-    plugins.oil.enable = true;
+			plugins.oil.enable = true;
 
-    plugins.treesitter = {
-      enable = true;
-      settings.highlight.enable = true;
-    };
+			plugins.treesitter = {
+				enable = true;
+				settings.highlight.enable = true;
+			};
 
-    plugins.treesitter-textobjects.enable = true;
-    plugins.typst-vim.enable = true;
-    plugins.which-key.enable = true;
-    plugins.nvim-autopairs.enable = true;
-    plugins.fidget.enable = true;
-    plugins.fugitive.enable = true;
-    plugins.auto-save.enable = true;
-    plugins.barbecue.enable=true;
-    plugins.git-conflict.enable=true;
-    plugins.gitsigns.enable=true;
-    plugins.headlines.enable=true;
-    plugins.hex.enable=true;
-    plugins.leap.enable=true;
-    plugins.luasnip.enable = true;
-    plugins.lightline.enable = true;
-    plugins.nix.enable = true;
-    plugins.nui.enable = true;
-    plugins.noice.enable = true;
-    plugins.notify.enable = true;
-    plugins.vim-surround.enable = true;
-  };
-}
+			plugins.treesitter-textobjects.enable = true;
+			plugins.typst-vim.enable = true;
+			plugins.which-key.enable = true;
+			plugins.nvim-autopairs.enable = true;
+			plugins.fidget.enable = true;
+			plugins.fugitive.enable = true;
+			plugins.auto-save.enable = true;
+			plugins.barbecue.enable=true;
+			plugins.git-conflict.enable=true;
+			plugins.gitsigns.enable=true;
+			plugins.headlines.enable=true;
+			plugins.hex.enable=true;
+			plugins.leap.enable=true;
+			plugins.luasnip.enable = true;
+			plugins.lightline.enable = true;
+			plugins.nix.enable = true;
+			plugins.nui.enable = true;
+			plugins.noice.enable = true;
+			plugins.notify.enable = true;
+			plugins.vim-surround.enable = true;
+		};
+	};
+	     }
--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixvim, flake-parts, ... }@inputs:
+    { nixvim, nixpkgs, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -20,10 +21,14 @@
       perSystem =
         { system, ... }:
         let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
           nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
           nixvimModule = {
             inherit system; # or alternatively, set `pkgs`
+            pkgs = pkgs;
             module = import ./config; # import the module directly
             # You can use `extraSpecialArgs` to pass additional arguments to your module files
             extraSpecialArgs = {
@@ -41,6 +46,7 @@
           packages = {
             # Lets you run `nix run .` to start nixvim
             default = nvim;
+            nixvim = nvim;
           };
         };
     };
